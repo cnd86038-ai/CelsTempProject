6242c4df7e1946ac466810f65c63ebee1be148c3
cnd86038-ai <cnd86038@email.vccs.edu>
2026-04-22 11:14:08 -0400

    Display information added along with main() and the fahrenheit loop.

--- CelsTempProject/main.cpp
+++ CelsTempProject/main.cpp
@@ -16,3 +16,33 @@ double getCelsius(double fahrenheit)
 {
 	return (5.0 / 9.0) * (fahrenheit - 32);
 }
+
+/*
+Purpose: Displays the fahrenheit to celsius conversion table
+Preconditions: fahrenheit is validated (>=0)
+Postconditions: displays the conversion table
+*/
+void displayTable()
+{
+	cout << "Fahrenheit\tCelsius" << endl;
+	cout << "-------------------------" << endl;
+
+	cout << setw(15) << "Fahrenheit"
+		<< setw(15) << "Celsius" << endl;
+
+
+	//Loop to display the conversion table
+	for (int fahrenheit = MIN_FAHRENHEIT; fahrenheit <= MAX_FAHRENHEIT; fahrenheit++)
+	{
+		double celsius = getCelsius(fahrenheit);
+		cout << setw(15) << fahrenheit
+			<< setw(15) << fixed << setprecision(1) << celsius << endl;
+	}
+}
+
+int main()
+{
+	displayTable();
+
+	return 0;
+}
